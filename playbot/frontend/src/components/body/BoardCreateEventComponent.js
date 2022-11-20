68409d3abf5b73c6eb1d1269fea42eb1e4553627
badazhkov <mikhail.badazhkov@sibers.com>
2022-11-20 10:36:52 +0300

    board create component for 376

--- playbot/frontend/src/components/body/BoardCreateEventComponent.js
+++ playbot/frontend/src/components/body/BoardCreateEventComponent.js
@@ -36,14 +36,21 @@ export default function BoardCreateEventComponent () {
 
     return (
         <div className={"board-create-event"}>
-            <span className={"elem elem-1"}>“Playbot” – твой проводник в мир футбола</span>
-            <div className={"elem elem-2"}>
-                <span className={"text black-cup-icon"}>Создавай события</span>
-                <span className={"text black-hands-icon"}>Находи соперников</span>
-                <span className={"text black-running-icon"}>Играй и развивай свой профиль</span>
+            <div className={"board-create-event-1280"}>
+                <span className={"elem elem-1"}>“Playbot” – твой проводник в мир футбола</span>
+                <div className={"elem elem-2"}>
+                    <span className={"text black-cup-icon"}>Создавай события</span>
+                    <span className={"text black-hands-icon"}>Находи соперников</span>
+                    <span className={"text black-running-icon"}>Играй и развивай свой профиль</span>
+                </div>
+                <div className={"elem elem-3"}>
+                    <button className={"btn"} onClick={getOpenCreateEvent}><div className={"white-ball-icon"}></div>Создать событие</button>
+                </div>
             </div>
-            <div className={"elem elem-3"}>
-                <button className={"btn"} onClick={getOpenCreateEvent}><div className={"white-ball-icon"}></div>Создать событие</button>
+
+            <div className={"board-create-event-376"}>
+                <span className={"elem elem-1 events-376-fon"}>События</span>
+                <button className={"elem elem-2 btn"} onClick={getOpenCreateEvent}><div className={"white-ball-icon"}></div></button>
             </div>
 
             <OpenCreateEventContext.Provider value={createEventWindow}>
