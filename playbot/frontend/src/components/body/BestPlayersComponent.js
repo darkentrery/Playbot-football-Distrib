c05f1ba18989adc51775c00e5f3fea10d0ed5117
badazhkov <mikhail.badazhkov@sibers.com>
2022-11-21 08:35:03 +0300

    players component

--- playbot/frontend/src/components/body/BestPlayersComponent.js
+++ playbot/frontend/src/components/body/BestPlayersComponent.js
@@ -16,40 +16,37 @@ export default function BestPlayersComponent () {
                 <span className={"elem country"}>Россия</span>
                 <span className={"elem city"}>Санкт-Петербург</span>
             </div>
-            <div className={"events-table"}>
+            <div className={"players-table"}>
                 <div className={"table-head"}>
-                    <span className={"elem elem-1"}>Название</span>
-                    <span className={"elem elem-2"}>Место проведения и дата начала</span>
-                    <span className={"elem elem-3"}>Кол-во участников</span>
-                    <span className={"elem elem-4"}>Средний рейтинг</span>
-                    <span className={"elem elem-5 gray-right-arrow-icon"}></span>
+                    <span className={"elem elem-1"}>Топ игроков</span>
+                    <span className={"elem elem-2"}>Рейтинг</span>
+                    <span className={"elem elem-3"}>% побед</span>
+                    <span className={"elem elem-4"}>Количество игр</span>
+                    <span className={"elem elem-5"}>Забитых мячей</span>
+                    <span className={"elem elem-6"}>Изменение рейтинга</span>
                 </div>
 
                 <div className={"table-head-376"}>
-                    <span className={"elem elem-1"}>Событие</span>
-                    <span className={"elem elem-2"}>Кол-во уч.</span>
-                    <span className={"elem elem-3"}>Рейтинг</span>
+                    <span className={"elem elem-1"}>Топ игроков</span>
+                    <span className={"elem elem-2"}>Рейтинг</span>
+                    <span className={"elem elem-3"}>% побед</span>
+                    <span className={"elem elem-4"}>Количество игр</span>
+                    <span className={"elem elem-5"}>Забитых мячей</span>
+                    <span className={"elem elem-6"}>Изменение рейтинга</span>
                 </div>
 
-                <div className={"date"}>
-                    <span className={"bold"}>12 мая <span>(вторник)</span></span>
-                </div>
-                <div className={"event"}>
-                    <span className={"elem elem-1 point-icon"}>Футбол с коллегами</span>
-                    <span className={"elem elem-2"}>ул. Тверская, 22 стр. 1<span className={"time"}>Событие началось, в 12:00</span></span>
+
+                <div className={"player"}>
+                    <span className={"elem elem-1 player-avatar-icon"}>1. <span className={"name"}>Андрей Иванов</span></span>
+                    <span className={"elem elem-2"}>354<span className={"green"}>&nbsp;+11</span></span>
                     <span className={"elem elem-3 green"}>10/10</span>
                     <span className={"elem elem-4 gray"}>88,9</span>
-                    <span className={"elem elem-5 gray-right-arrow-icon"}></span>
-                </div>
-                <div className={"event"}>
-                    <span className={"elem elem-1 star-icon"}>Футбол с коллегами</span>
-                    <span className={"elem elem-2"}>ул. Тверская, 22 стр. 1<span className={"time"}>Событие началось, в 12:00</span></span>
-                    <span className={"elem elem-3 red"}>10/10</span>
-                    <span className={"elem elem-4 orange"}>88,9</span>
-                    <span className={"elem elem-5 gray-right-arrow-icon"}></span>
+                    <span className={"elem elem-5 "}>27</span>
+                    <span className={"elem elem-6 rank-icon"}></span>
                 </div>
 
-                <div className={"event-376"}>
+
+                <div className={"player-376"}>
                     <div className={"row row-1"}>
                         <span className={"elem elem-1 point-icon"}>Футбол с коллегами<span className={"gray"}>12:00</span></span>
                         <span className={"elem elem-2 red"}>10/10</span>
@@ -59,7 +56,7 @@ export default function BestPlayersComponent () {
                         <span className={"elem elem-1 map-point-icon"}>ул. Садово-Кудринская, 19 стр. 1</span>
                     </div>
                 </div>
-                <div className={"event-376"}>
+                <div className={"player-376"}>
                     <div className={"row row-1"}>
                         <span className={"elem elem-1 star-icon"}>Футбол с коллегами<span className={"gray"}>12:00</span></span>
                         <span className={"elem elem-2 red"}>10/10</span>
