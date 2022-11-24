03b28cd0fa852e80898f8440c0bfdd9864a5fde0
badazhkov <mikhail.badazhkov@sibers.com>
2022-11-24 06:51:22 +0300

    event description for 744

--- playbot/frontend/src/components/body/EventDescriptionComponent.js
+++ playbot/frontend/src/components/body/EventDescriptionComponent.js
@@ -5,24 +5,35 @@ export default function EventDescriptionComponent () {
 
     return (
         <div className={"event-description-component"}>
-            <div className={"elem elem-1"}>
+            <div className={"elem-1280 elem-1"}>
                 <span className={"el black-edit-icon link"}>Редактировать игру</span>
                 <span className={"el gray-copy-icon link"}>Копировать ссылку</span>
             </div>
-            <span className={"elem elem-2"}>Информация</span>
-            <span className={"elem elem-3 dark-gray-comment-icon"}>Всем привет! После игры просьба не расходиться, будет фотосессия.</span>
-            <div className={"elem elem-4"}>
+            <span className={"elem-1280 elem-2"}>Информация</span>
+            <span className={"elem-1280 elem-3 dark-gray-comment-icon"}>Всем привет! После игры просьба не расходиться, будет фотосессия.</span>
+            <div className={"elem-1280 elem-4"}>
                 <span className={"el el-1"}>Адрес:</span>
                 <span className={"el el-2"}>Организатор:</span>
             </div>
-            <div className={"elem elem-5"}>
+            <div className={"elem-1280 elem-5"}>
                 <span className={"el el-1"}>Москва, ЦАО, Тверской район, ул. Тверская, 22 стр. 1</span>
                 <span className={"el el-2"}>Андрей Иванов</span>
             </div>
-            <div className={"elem elem-6"}>
+            <div className={"elem-1280 elem-6"}>
 
             </div>
 
+            <div className={"elem-744 elem-1"}>
+                <span className={"el black-edit-icon link"}>Редактировать игру</span>
+                <span className={"el gray-copy-icon link"}></span>
+            </div>
+            <span className={"elem-744 elem-2"}>Информация</span>
+            <span className={"elem-744 elem-3 dark-gray-comment-icon"}>Всем привет! После игры просьба не расходиться, будет фотосессия.</span>
+            <span className={"elem-744 elem-4"}>Организатор:</span>
+            <span className={"elem-744 elem-5"}>Андрей Иванов</span>
+            <span className={"elem-744 elem-6"}>Адрес:</span>
+            <span className={"elem-744 elem-7"}>Москва, ЦАО, Тверской район, ул. Тверская, 22 стр. 1</span>
+            <div className={"elem-744 elem-8"}></div>
         </div>
     )
 }
