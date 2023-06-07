1a50ebf9000cbdf295458a65147860f28b74e136
darkentrery <badajkovmihail@gmail.com>
2023-06-07 19:06:01 +0300

    fix team component

--- frontend/src/components/teamNameComponent/TeamNameComponent.js
+++ frontend/src/components/teamNameComponent/TeamNameComponent.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 
 
 export const TeamNameComponent = ({
@@ -7,6 +7,13 @@ export const TeamNameComponent = ({
     setValue,
 }) => {
     const [isInput, setIsInput] = useState(false);
+    const ref = useRef(null);
+
+    useEffect(() => {
+        if (isInput) {
+            ref.current.querySelector('input').focus();
+        }
+    }, [isInput])
 
     const changeValue = (e) => {
         setValue(e.target.value.slice(0, 20));
@@ -16,10 +23,22 @@ export const TeamNameComponent = ({
         setIsInput(!isInput);
     }
 
+    document.addEventListener('click', (e) => {
+        if (e.target !== ref.current && e.target.parentNode !== ref.current) {
+            setIsInput(false);
+        }
+    })
+
+    const clickEnter = (e) => {
+        if (e.keyCode === 13) {
+            setIsInput(false);
+        }
+    }
+
     return (
-        <div className={`team-name-component ${className}`}>
+        <div className={`team-name-component ${className}`} ref={ref}>
             <span className={`black-600-16 ${isInput ? 'hidden' : ''}`}>{value}</span>
-            <input className={`input ${isInput ? '' : 'hidden'}`} type="text" value={value} onChange={changeValue}/>
+            <input className={`input ${isInput ? '' : 'hidden'}`} type="text" value={value} onChange={changeValue} onKeyUp={clickEnter}/>
             <div className={"pencil-icon"} onClick={clickPencil}></div>
         </div>
     )
