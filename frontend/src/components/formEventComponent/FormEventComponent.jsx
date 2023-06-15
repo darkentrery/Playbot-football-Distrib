7d24cb686c1a5b16077012bef015a1771a155ecb
zagrebaliakskill <orshan1003@gmail.com>
2023-06-15 08:58:04 +0300

    fix age limit input

--- frontend/src/components/formEventComponent/FormEventComponent.jsx
+++ frontend/src/components/formEventComponent/FormEventComponent.jsx
@@ -384,7 +384,7 @@ export const FormEventComponent = ({
                     </div>
                     <div className="formEvent__age-limit">
                         <span className="text-footnote">Возраст</span>
-                        <InputFromToComponent isError={ageLimitError} setValue={setAgeLimit} value1={ageLimit[0]} value2={ageLimit[1]} classes={'formEvent__age-limit-input'} />
+                        <InputFromToComponent isError={ageLimitError} setValue={setAgeLimit} value1={event.min_age ? event.min_age : 0} value2={event.max_age ? event.max_age : 0} classes={'formEvent__age-limit-input'} />
                     </div>
                 </div>
                 <div className="formEvent__rating-range">
