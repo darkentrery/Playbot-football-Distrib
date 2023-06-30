c87dd89c9b35f742df1fc09dce075751733e22ce
zagrebaliakskill <orshan1003@gmail.com>
2023-07-01 02:58:36 +0300

    .

--- frontend/src/components/FaqItemList/FaqItemList.jsx
+++ frontend/src/components/FaqItemList/FaqItemList.jsx
@@ -4,8 +4,8 @@ export const FaqItemList = ({children}) => {
     return (
         <> 
         <ul className="faq-items__list">
-            {children.map(child => (
-                <li className="faq-items__list-item">
+            {children.map((child, i) => (
+                <li className="faq-items__list-item" key={i}>
                     {child}
                 </li>
             ))}
