75554a9f76b77101d029b62bfe918b95bd1fe901
zagrebaliakskill <orshan1003@gmail.com>
2023-07-11 15:21:24 +0300

    profile skeletons mobile

--- frontend/src/components/pages/myProfileComponent/MyProfileComponent.jsx
+++ frontend/src/components/pages/myProfileComponent/MyProfileComponent.jsx
@@ -1,5 +1,6 @@
 import VisibleProfileWrapper from "../../../redux/containers/VisibleProfileWrapper";
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 import { UserProfileHeader } from "../../UserProfileHeader/UserProfileHeader";
 import { UserProfilePins } from "../../UserProfilePins/UserProfilePins";
@@ -10,12 +11,14 @@ import { UserProfileGameHistory } from "../../UserProfileGameHistory/UserProfile
 import { UserProfileSamePlayers } from "../../UserProfileSamePlayers/UserProfileSamePlayers";
 import { useSelector } from "react-redux";
 import { UserProfileFutureGames } from "../../UserProfileFutureGames/UserProfileFutureGames";
+import { MyProfileSkeletons376 } from "../../MyProfileSkeletons/MyProfileSkeletons376";
 
 export const MyProfileComponent = ({
     player,
     user,
     funcs,
 }) => {
+    const [isLoading, setIsLoading] = useState(true)
 
     const showMenu = () => {
         funcs.openShowMenu();
@@ -28,52 +31,65 @@ export const MyProfileComponent = ({
         isProfileOwner = true;
     }
 
+    // useEffect(() => {
+    //     if (player) {
+    //         setIsLoading(false)
+    //     }
+    // }, [player])
     return (
-        <VisibleProfileWrapper>
-            {isProfileOwner
-                ? <UserProfileNavMobile type="myProfile" />
-                : <UserProfileNavMobile type="onlyArrow" />
+        <>
+            {isLoading ?
+                <VisibleProfileWrapper>
+                    <MyProfileSkeletons376/>
+                </VisibleProfileWrapper>
+                :
+                <VisibleProfileWrapper>
+                    {isProfileOwner
+                        ? <UserProfileNavMobile type="myProfile" />
+                        : <UserProfileNavMobile type="onlyArrow" />
+                    }
+
+                    <UserProfileHeader
+                        username={event.player.username}
+                        photo={event.player.small_card_photo} rating={event.player.rank}
+                        ratingPlace={event.player.ranking_place}
+                        age={event.player.birthday}
+                        isProfileOwner={isProfileOwner}
+                    />
+
+                    {!isProfileOwner && (
+                        <UserProfilePins
+                            dateJoined={event.player.date_joined}
+                            playedTogether={"need backend"}
+                            totalGames={event.player.all_games}
+                        />
+                    )}
+
+                    <UserProfileStats
+                        wins={event.player.wins}
+                        draw={"need backend"}
+                        lose={event.player.loss}
+                        winRate={event.player.wins_percent}
+                        goals={event.player.count_goals}
+                        assists={event.player.count_assist}
+                        totalGames={event.player.all_games}
+                    />
+
+                    {isProfileOwner && (
+                        <UserProfileFutureGames events={event.player.event_player} />
+                    )}
+
+                    <UserProfileAchievements />
+
+                    <UserProfileGameHistory
+                        events={event.player.event_player}
+                    />
+
+                    <UserProfileSamePlayers
+                        users={event.player.same_players}
+                    />
+                </VisibleProfileWrapper>
             }
-
-            <UserProfileHeader
-                username={event.player.username}
-                photo={event.player.small_card_photo} rating={event.player.rank}
-                ratingPlace={event.player.ranking_place}
-                age={event.player.birthday}
-                isProfileOwner={isProfileOwner}
-            />
-
-            {!isProfileOwner && (
-                <UserProfilePins
-                    dateJoined={event.player.date_joined}
-                    playedTogether={"need backend"}
-                    totalGames={event.player.all_games}
-                />
-            )}
-
-            <UserProfileStats
-                wins={event.player.wins}
-                draw={"need backend"}
-                lose={event.player.loss}
-                winRate={event.player.wins_percent}
-                goals={event.player.count_goals}
-                assists={event.player.count_assist}
-                totalGames={event.player.all_games}
-            />
-
-            {isProfileOwner && (
-                <UserProfileFutureGames events={event.player.event_player} />
-            )}
-
-            <UserProfileAchievements />
-
-            <UserProfileGameHistory
-                events={event.player.event_player}
-            />
-
-            <UserProfileSamePlayers
-                users={event.player.same_players}
-            />
-        </VisibleProfileWrapper>
+        </>
     )
 }
